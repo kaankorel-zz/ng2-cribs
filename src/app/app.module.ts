0ec4b602a9073510a457df5572548a7b3b2511a2
Kaan Korel <kaan@korelblog.com>
2019-01-21 10:49:46 +0300

    Update3-7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CribCardComponent } from './crib-card/crib-card.component';
+import { CribListingComponent } from './crib-listing/crib-listing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CribCardComponent,
+    CribListingComponent
   ],
   imports: [
     BrowserModule,
