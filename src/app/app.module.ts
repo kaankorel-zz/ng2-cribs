777345f9eba829122652806c82597c90bc95a310
Kaan Korel <kaan@korelblog.com>
2019-01-21 18:02:49 +0300

    Update 3 - 11 ( Form Data )

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 
@@ -8,15 +10,18 @@ import { CribCardComponent } from './crib-card/crib-card.component';
 import { CribListingComponent } from './crib-listing/crib-listing.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CribsService } from './services/cribs.service';
+import { AddListingFormComponent } from './add-listing-form/add-listing-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CribCardComponent,
-    CribListingComponent
+    CribListingComponent,
+    AddListingFormComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     HttpModule,
     AppRoutingModule,
     NgbModule.forRoot()
